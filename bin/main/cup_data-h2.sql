82b17ffbf1df762625a4efe90efde61fa754b087
WILDD0G\hyeonjin <peslapimpact@gmail.com>
2019-08-09 00:16:20 +0900

    mod cup

--- bin/main/cup_data-h2.sql
+++ bin/main/cup_data-h2.sql
@@ -1,6 +1,7 @@
 insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('shaolin_temple.png', '제2회 소림사컵', 128, 'black', '', now(), now(), now(), now());
-insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('pfc.png', '제2회 기업 최강자전', 32, 'silver', '', now(), now(), now(), now());
-insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('shaolin_temple.png', '제1회 소림사컵', 64, 'gold', '', now(), now(), now(), now());
 insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('pfc.png', 'PFC', 102, 'black', '', now(), now(), now(), now());
-insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('shaolin_temple.png', '클랜 챔피언스 2020', 32, '', 'black', now(), now(), now(), now());
+insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('pes_korea.jpg', '제2회 기업 최강자전', 32, 'silver', '', now(), now(), now(), now());
+insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('basic_2.jpg', '클랜 챔피언스 2020', 32, '', 'black', now(), now(), now(), now());
+insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('shaolin_temple.png', '제1회 소림사컵', 64, 'gold', '', now(), now(), now(), now());
+insert into cup (img, name, participants, grade, introduce, date_from, date_to, created_date, modified_date) values ('basic.jpg', '라프배 2020', 32, '', 'black', now(), now(), now(), now());
 
