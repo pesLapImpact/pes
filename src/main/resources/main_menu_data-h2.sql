f82f1809fe4bdcc5a3e43c93451c3bc2553758ad
lapimpact2020 <wildhyeonjin@gmail.com>
2021-02-03 10:48:09 +0900

    add portfolio

--- src/main/resources/main_menu_data-h2.sql
+++ src/main/resources/main_menu_data-h2.sql
@@ -1,3 +1,3 @@
-insert into main_menu (link, img, main_caption, sub_caption, created_date, modified_date) values ('peslapimpact', 'pes.jpg', 'Winning Eleven Fan Page', 'Check the Your Winning Eleven Rating', now(), now());
 insert into main_menu (link, img, main_caption, sub_caption, created_date, modified_date) values ('portfolio', 'portfolio.jpg', 'Lap Impact Portfolio', '', now(), now());
+insert into main_menu (link, img, main_caption, sub_caption, created_date, modified_date) values ('peslapimpact', 'pes.jpg', 'Winning Eleven Fan Page', 'Check the Your Winning Eleven Rating', now(), now());
 insert into main_menu (link, img, main_caption, sub_caption, created_date, modified_date) values ('lapimpact', 'lapimpact.jpg', 'Lap Impact Routine', 'Show you Lap Impact Routine', now(), now());
